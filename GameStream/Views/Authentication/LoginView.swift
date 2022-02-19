c7b8c5fc6738c9eebc7506f6fc414e17d4a79113
Uriel Enrique <urielenrique99@gmail.com>
2022-02-19 15:38:27 -0600

    login view done

--- GameStream/Views/Authentication/LoginView.swift
+++ GameStream/Views/Authentication/LoginView.swift
@@ -29,6 +29,9 @@ struct LoginView: View {
                     .background(Color("DarkCyanColor"))
                     .padding(.bottom)
                 
+                Text("Escribe tu contraseña")
+                    .foregroundColor(.white)
+                    .foregroundColor(Color("DarkCyanColor"))
                 ZStack(alignment: .leading) {
                     if password.isEmpty {
                         Text("Escribe tu contraseña")
@@ -41,13 +44,80 @@ struct LoginView: View {
                 Divider().frame(height: 1)
                     .background(Color("DarkCyanColor"))
                     .padding(.bottom)
+                
+                
+                Text("¿Olvidaste tu contraseña?")
+                    .font(.footnote)
+                    .frame(width: 300, alignment: .trailing)
+                    .foregroundColor(Color("DarkCyanColor"))
+                    .padding(.bottom)
+                
+                LoginButtonsModule()
             }.padding(.horizontal, 77)
         }
+    }
+}
+
+struct LoginButtonsModule: View {
+    var body: some View {
+        Button {
+            login()
+        } label: {
+            Text("INICIAR SESIÓN")
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 6)
+                        .stroke(Color("DarkCyanColor"), lineWidth: 1)
+                        .shadow(color: .white, radius: 6)
+                )
+        }
+        Spacer(minLength: 100)
+        Text("Inicia sesión con redes sociales")
+            .font(.headline)
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity, alignment: .center)
+        
+        HStack {
+            Button {
+                print("Facebook login")
+            } label: {
+                Text("Facebook")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+                    .background(Color("FacebookLoginColor"))
+                    .cornerRadius(10)
+
+            }
+            Button {
+                print("Twitter login")
+            } label: {
+                Text("Twitter")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+                    .background(Color("FacebookLoginColor"))
+                    .cornerRadius(10)
+            }
+
+        }.frame(maxWidth: .infinity, alignment: .center)
+
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        Image("pantalla1")
     }
+}
+
+
+func login() {
+    print("Login")
 }
