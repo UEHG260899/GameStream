30cbc71fbdf552a4fac3a2d98669ce536804f779
Uriel Enrique <urielenrique99@gmail.com>
2022-02-19 13:57:08 -0600

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Uriel Hernandez Gonzalez on 19/02/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
