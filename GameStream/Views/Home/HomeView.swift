79029350ac3a68ef67ad19531fc99bdfc9861fda
Uriel Enrique <urielenrique99@gmail.com>
2022-02-19 17:56:55 -0600

    fixing iOS 15 default tabBar config

--- GameStream/Views/Home/HomeView.swift
+++ GameStream/Views/Home/HomeView.swift
@@ -6,38 +6,52 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct HomeView: View {
+    
+    @State var selectedTab: Int = 2
+    
+    init() {
+        let appearance = UITabBarAppearance()
+        appearance.backgroundColor = UIColor(named: "TabBarColor")
+        if #available(iOS 15.0, *) {
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
+    }
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             Text("Perfil")
                 .font(.system(size: 30 , weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "person")
                     Text("Perfil")
-                }
-            
+                }.tag(0)
+
             Text("Juegos")
                 .font(.system(size: 30 , weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "gamecontroller")
                     Text("Juegos")
-                }
+                }.tag(1)
             Text("Home")
                 .font(.system(size: 30 , weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "house")
                     Text("Inicio")
-                }
-            
+                }.tag(2)
+
             Text("Favoritos")
                 .font(.system(size: 30 , weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "heart")
                     Text("Favoritos")
-                }
+                }.tag(3)
         }
     }
+    
+    
 }
 
 struct HomeView_Previews: PreviewProvider {
