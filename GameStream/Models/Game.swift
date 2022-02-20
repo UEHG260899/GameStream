e5b6b3a4981bb259011c21d1d2c82985a0ba4837
Uriel Enrique <urielenrique99@gmail.com>
2022-02-20 15:02:23 -0600

    created models

--- GameStream/Models/Game.swift
+++ GameStream/Models/Game.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct Games: Codable {
+    var games: [Game]
+}
+struct Game: Codable {
+    var title: String
+    var studio: String
+    var contentRating: String
+    var publicationYear: String
+    var description: String
+    var platforms: [String]
+    var tags: [String]
+    var videosUrl: VideoUrl
+    var galleryImages: [String]
+}
+
+struct VideoUrl: Codable {
+    var mobile: String
+    var table: String
+}
